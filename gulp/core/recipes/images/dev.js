c9e8208b51575bddb6b8ea7a4fb0bbfea28b918e
Daniels Jirgensons <djirgensons@evolution.com>
2025-08-13 16:17:06 +0300

    imagemin image optimisation script

--- gulp/core/recipes/images/dev.js
+++ gulp/core/recipes/images/dev.js
@@ -1,61 +1,48 @@
 const gulp = require('gulp');
 const plumber = require('gulp-plumber');
-// TODO: var squoosh = require('gulp-squoosh');
 const notify = require('gulp-notify');
 const browserSync = require('browser-sync');
-const path = require('path');
+const mergeStream = require('merge-stream');
 
-// utils
 const pumped = require('../../utils/pumped');
-
-// config
 const config = require('../../config/images');
 
-/**
- * Move Images to
- * the built theme
- *
- */
-module.exports = function () {
-	return gulp.src(config.paths.src)
+module.exports = async function () {
+	const imagemin = (await import('gulp-imagemin')).default;
+	const imageminMozjpeg = (await import('imagemin-mozjpeg')).default;
+	const imageminOptipng = (await import('imagemin-optipng')).default;
+	const imageminGifsicle = (await import('imagemin-gifsicle')).default;
+	const imageminSvgo = (await import('imagemin-svgo')).default;
+
+	// JPG stream
+	const jpgStream = gulp.src(config.paths.src)
 		.pipe(plumber())
-		// .pipe(
-		// 	squoosh(({ width, height, size, filePath }) => {
-		// 		let options = {
-		// 			encodeOptions: { mozjpeg: {} }
-		// 		};
+		.pipe(imagemin([
+			imageminMozjpeg({ quality: 80 })
+		]))
+		.pipe(gulp.dest(config.paths.dest));
 
-		// 		if (path.extname(filePath) === '.jpg') {
-		// 			options = {
-		// 				encodeOptions: {
-		// 					webp: {},
-		// 					mozjpeg: {},
-		// 				},
-		// 			};
-		// 		}
+	// PNG stream
+	const pngStream = gulp.src(config.paths.src)
+		.pipe(plumber())
+		.pipe(imagemin([
+			imageminOptipng({ optimizationLevel: 5 })
+		]))
+		.pipe(gulp.dest(config.paths.dest));
 
-		// 		if (path.extname(filePath) === '.png') {
-		// 			options = {
-		// 				encodeOptions: {
-		// 					oxipng: {},
-		// 				},
-		// 				preprocessOptions: {
-		// 					quant: {
-		// 						enabled: true,
-		// 						numColors: 16,
-		// 					},
-		// 				},
-		// 			};
-		// 		}
+	// GIF/SVG stream
+	const gifSvgStream = gulp.src(config.paths.src)
+		.pipe(plumber())
+		.pipe(imagemin([
+			imageminGifsicle({ interlaced: true }),
+			imageminSvgo()
+		]))
+		.pipe(gulp.dest(config.paths.dest));
 
-		// 		return options;
-		// 	})
-		// )
-		.pipe(gulp.dest(config.paths.dest))
+	return mergeStream(jpgStream, pngStream, gifSvgStream)
 		.pipe(notify({
-			"message": pumped("Images Moved"),
-			"onLast": true
+			message: pumped("Images optimized"),
+			onLast: true
 		}))
-
 		.on('end', browserSync.reload);
 };
